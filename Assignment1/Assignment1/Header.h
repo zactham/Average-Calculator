d9f6f79c90522e661100c73b6b2b7e4119a601e8
zactham <zactham@gmail.com>
2019-01-17 13:05:57 -0500

    Implemented cleaner way to caluclate average

--- Assignment1/Assignment1/Header.h
+++ Assignment1/Assignment1/Header.h
@@ -1,12 +1,14 @@
 #pragma once
 
-int average(int nums[])
+float average(float nums[], int size)
 {
-	int size = nums.size();
-	int total = 0;
+	float total = 0;
+	//Runs through every number adding it to the total
 	for (int index = 0; index < size; index++) 
 	{
 		total += nums[index];
+		
 	}
+	//Returns the average of the data: sum of the numbers divided by the amount of numbers
 	return (total / size);
 }
--- Assignment1/Assignment1/Main.cpp
+++ Assignment1/Assignment1/Main.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
+#include "Header.h"
 
-int average(int nums[]);
+float average(float nums[], int size);
+
 
 int main()
 {
 	int size = 1;
 	float total = 0;
-	float average = 0;
+
+	//Usser input
 	std::cout << "How many numbers would you like to enter?" << std::endl;
 	std::cin >> size;
+
+	//Creates array for all of the numbers
+	float *nums = new float [size];
+
+	//Puts each number in the array as it is entered
 	for (int index = 0; index < size; index++)
 	{
 		std::cout << "Number: ";
-		int num = 0;
-		std::cin >> num;
-		total = total + num;
+		std::cin >> nums[index];
 	}
-	std::cout << "Calculating: ";
-	average = total / size;
-	std::cout << "Average of the data: " << average << std::endl;
+	//Calculates the average of the data set by calling the average method in the header class
+	std::cout << "Average of the data: " << average(nums, size) << std::endl;
 	std::cin >> size;//just so the file does not close
+	
 	
 }
