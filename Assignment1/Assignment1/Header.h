24069d2f517fe6cc5e61381dbd736fe30f932723
zactham <zactham@gmail.com>
2019-01-17 15:20:11 -0500

    Created a user input method and cleaned up header file

--- Assignment1/Assignment1/Header.h
+++ Assignment1/Assignment1/Header.h
@@ -1,14 +1,4 @@
 #pragma once
 
-float average(float nums[], int size)
-{
-	float total = 0;
-	//Runs through every number adding it to the total
-	for (int index = 0; index < size; index++) 
-	{
-		total += nums[index];
-		
-	}
-	//Returns the average of the data: sum of the numbers divided by the amount of numbers
-	return (total / size);
-}
+float average(float nums[], int size);
+void userInput();
--- Assignment1/Assignment1/Main.cpp
+++ Assignment1/Assignment1/Main.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
 #include "Header.h"
 
-float average(float nums[], int size);
+int size = 1;
+float total = 0;
+
+//Creates array for all of the numbers
+float *nums = new float[size];
 
 
 int main()
 {
-	int size = 1;
-	float total = 0;
+	userInput();
+	//Calculates the average of the data set by calling the average method in the header class
+	std::cout << "Average of the data: " << average(nums, size) << std::endl;
+	std::cin >> size;//just so the file does not close
+}
 
+float average(float nums[], int size)
+{
+	float total = 0;
+	//Runs through every number adding it to the total
+	for (int index = 0; index < size; index++)
+	{
+		total += nums[index];
+	}
+	//Returns the average of the data: sum of the numbers divided by the amount of numbers
+	return (total / size);
+}
+
+void userInput() 
+{
 	//Usser input
 	std::cout << "How many numbers would you like to enter?" << std::endl;
 	std::cin >> size;
-
-	//Creates array for all of the numbers
-	float *nums = new float [size];
 
 	//Puts each number in the array as it is entered
 	for (int index = 0; index < size; index++)
@@ -22,9 +40,7 @@ int main()
 		std::cout << "Number: ";
 		std::cin >> nums[index];
 	}
-	//Calculates the average of the data set by calling the average method in the header class
-	std::cout << "Average of the data: " << average(nums, size) << std::endl;
-	std::cin >> size;//just so the file does not close
-	
-	
+
+
+
 }
