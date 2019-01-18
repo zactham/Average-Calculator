7b2e82e7364b27192de6871ae8714e00e31785dd
zactham <zactham@gmail.com>
2019-01-17 22:23:24 -0500

    Created a class and ran through its object

--- Assignment1/Assignment1/Header.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-float average(float nums[], int size);
-int userInputSize();
-float* userInputNumbers(int size);
--- Assignment1/Assignment1/Main.cpp
+++ Assignment1/Assignment1/Main.cpp
@@ -1,50 +1,10 @@
 #include <iostream>
-#include "Header.h"
+#include "AverageCalculator.h"
 
 
 int main()
 {
-	int size = userInputSize();
-	
-	//Calculates the average of the data set by calling the average method in the header class
-	std::cout << "Average of the data: " << average(userInputNumbers(size), size) << std::endl;
-	//delete nums;
-	int temp;
-	std::cin >> temp;//just so the file does not close
+	AverageCalculator* calc = new AverageCalculator();	
 }
 
-float average(float *nums, int size)
-{
-	float total = 0;
-	//Runs through every number adding it to the total
-	for (int index = 0; index < size; index++)
-	{
-		total += *(nums + index);
-	}
-	//Returns the average of the data: sum of the numbers divided by the amount of numbers
-	return (total / size);
-}
 
-int userInputSize()
-{
-	//Usser input
-	std::cout << "How many numbers would you like to enter?" << std::endl;
-	int size = 0;
-	std::cin >> size;
-
-	return size;
-}
-
-float* userInputNumbers(int size)
-{
-	//Creates array for all of the numbers
-	float* nums = new float[size];
-	//Puts each number in the array as it is entered
-	for (int index = 0; index < size; index++)
-	{
-		std::cout << "Number: ";
-		std::cin >> nums[index];
-	}
-
-	return nums;
-}
